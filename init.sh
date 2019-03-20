0b2e11b66da41cfbab036e7d8c911d8657ae40ec
Nikita Deputatov <deputatovn@yandex.ru>
2019-03-20 21:01:32 +0300

    Отредактирован файл init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo unlink /etc/nginx/sites-enabled/default 
 sudo /etc/init.d/nginx restart
+﻿sudo /etc/init.d/mysql start﻿
