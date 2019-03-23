97d972c3b70677ad2868bfc0030e84c211b51a74
Nikita Deputatov <deputatovn@yandex.ru>
2019-03-23 19:55:57 +0300

    Создан WSGI hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
 def wsgi_application(environ, start_response):
-	status = '200 OK'
-	headers = [
-		('Content-Type', 'text/plain')
-	]
-	body = '' #abc
-	start_response(status, headers)
-	return [body]
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
