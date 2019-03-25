95332406a5e15df9e8d20e4f7a3623b9881d378a
Nikita Deputatov <deputatovn@yandex.ru>
2019-03-25 17:26:46 +0300

    Отредактирован файл init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
-cd /home/box/web && sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
+cd /home/box/web && sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:wsgi_application
 sudo /etc/init.d/gunicorn restart
