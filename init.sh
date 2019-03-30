774da3d7a098e059c6f43e109e3dd352295d9070
Nikita Deputatov <deputatovn@yandex.ru>
2019-03-30 15:52:12 +0300

    Изменил *.conf файлы

--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
 # settings for nginx
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo service nginx stop
-sudo nginx -c /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
 
 # settings for gunicorn
-sudo ln -s /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
-#sudo gunicorn -b 0.0.0.0:8080 gunicorn hello:application
-cd /home/box/web && sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:wsgi_application
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
